33d15be33ef1d50ca8c3b5340260d9e66c819745
senaneves <senaneves@gmail.com>
2022-03-04 12:07:03 -0300

    Implementandoa Arvore Binaria

---
+++ src/main/java/com/dio/estruturaDados/arvoreBinaria/model/ObjArvore.java
@@ -0,0 +1,10 @@
+package com.dio.estruturaDados.arvoreBinaria.model;
+
+public abstract class   ObjArvore<T> implements Comparable<T>{
+
+    public abstract boolean equals(Object o);
+    public abstract int hashCode();
+    public abstract int compareTo(T outro);
+    public abstract String toString();
+
+}
